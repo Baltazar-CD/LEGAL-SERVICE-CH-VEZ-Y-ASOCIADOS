bf926d4bf01bc4b9a01f335cd5282981492b231a
Campos Delgado Baltazar <91445820+Baltazar-CD@users.noreply.github.com>
2022-05-02 21:38:34 -0600

    Add files via upload

---
+++ assets/js/video.js
@@ -0,0 +1,18 @@
+$('#play-video').on('click', function(e){
+  e.preventDefault();
+  $('#video-overlay').addClass('open');
+  $("#video-overlay").append('<iframe width="560" height="315" src="assets/images/274090711_2988608204785293_6954760201129220111_n.mp4" frameborder="0" allow="accelerometer; autoplay=1; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function(e){
+  e.preventDefault();
+  close_video();
+});
+
+$(document).keyup(function(e){
+  if(e.keyCode === 27) { close_video(); }
+});
+
+function close_video() {
+  $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
